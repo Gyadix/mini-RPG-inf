ba542b4f9f31a043f16acce87707ad90b3e7239b
Gyadix <giada.roncari2009@gmail.com>
2026-01-30 14:59:19 +0100

    Update rewritten_code.java

--- rewritten_code.java
+++ rewritten_code.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 
 public class Main{
+    static String nome;
+    static String scelta1 = "Inizia";
+    static String scelta2 = "Inventario";
+    static String scelta3 = "Esci";
+    static int scelta;
+    static int classe;
+    static boolean giocoIniziato = false;
+    static int ps_arciere = 15;
+    static int forza_arciere = 15;
+    static int vigore_arciere = 20;
+    static int ps_fante = 20;
+    static int forza_fante = 25;
+    static int vigore_fante = 15;
+    static String cavallo;
     
     public static void main(String[]args){
         try (Scanner input = new Scanner(System.in)){
             
-            String scelta1 = "Inizia";
-            String scelta2 = "Inventario";
-            String scelta3 = "Esci";
-            
-            int scelta;
-            int classe;
-            boolean giocoIniziato = false;
-            
             System.out.print("Inserisci nome eroe: ");
-            String nome = input.nextLine();
+            nome = input.nextLine();
         }
+    }
             
-            public static void menu(){
+    public static void menu(){
                 System.out.println("Benvenuto/a "+nome);
                 System.out.println("1. "+scelta1);
                 System.out.println("2. "+scelta2);
@@ -48,24 +55,18 @@ public class Main{
             }
             switch (classe){
                 case 1:
-                int ps = 15;
-                int forza = 15;
-                int vigore = 20;
                 System.out.println("Hai scelto l'arciere");
-                System.out.println("Punti salute: " + ps + "\nforza: " + forza + "\nVigore: " + vigore);
+                System.out.println("Punti salute: " + ps_arciere + "\nforza: " + forza_arciere + "\nVigore: " + vigore_arciere);
                 System.out.println("La tua arma è un arco da guerra");
                 giocoIniziato = true;
                 break;
                 
                 case 2:
-                int ps = 20;
-                int forza = 25;
-                int vigore = 15;
                 System.out.println("Hai scelto il fante");
-                System.out.println("Punti salute: " + ps + "\nforza: " + forza + "\nVigore: " + vigore);
+                System.out.println("Punti salute: " + ps_fante + "\nforza: " + forza_fante + "\nVigore: " + vigore_fante);
                 System.out.println("La tua arma è una lancia ed hai un cavallo");
                 System.out.print("Scegli il nome del cavallo: ");
-                String cavallo = input.nextLine();
+                cavallo = input.nextLine();
                 System.out.println("Il tuo cavallo si chiama: " + cavallo);
                 giocoIniziato = true;
                 break;
